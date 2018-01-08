285450040bee6f9128845b55629762ce6f0b646e
Niennis <estefania.osses.v@gmail>
2018-01-08 17:49:43 -0300

    Etapa 8: Quita y agrega clase make a page.recipe

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,12 @@ $(document).ready( function(){
   $('.js-menu').hide();
 
   console.log('Documento listo. Paso 7');
+
+  $('.js-show-recipe').click(function(){
+    $('.page.recipe').removeClass('make');    
+  });
+
+  $('.js-show-make').click(function(){
+    $('.page.recipe').addClass('make');
+  });
 });
